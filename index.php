389d3093adf70336a7f7be73cd1caee6908afce4
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-08-07 03:01:17 +0800

    Design pagination system in css

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Pagination</title>
+</head>
+<body>
+    <div class="page-wrapper">
+        <div class="post-list">
+            <h1>Articles</h1>
+            <article>
+                <h3>Lorem ipsum dolor sit amet.</h3>
+            </article>
+            <article>
+                <h3>Lorem ipsum dolor sit amet.</h3>
+            </article>
+            <article>
+                <h3>Lorem ipsum dolor sit amet.</h3>
+            </article>
+            <article>
+                <h3>Lorem ipsum dolor sit amet.</h3>
+            </article>
+            <article>
+                <h3>Lorem ipsum dolor sit amet.</h3>
+            </article>
+        </div>
+        <div class="pagination-links">
+            <a href="#" class="btn disabled"><ion-icon name="arrow-back-outline"></ion-icon> Prev Page</a>
+            <a href="#" class="btn">Next Page <ion-icon name="arrow-forward-outline"></ion-icon></a>
+        </div>
+    </div>
+
+    <!-- ionic icons -->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
